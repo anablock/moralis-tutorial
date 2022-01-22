4bba46f2d67c5373d60cd4878e45c83ca368b0a5
Vuk <vuchina@icloud.com>
2022-01-21 16:16:52 -0800

    add

--- main.js
+++ main.js
@@ -3,6 +3,29 @@ const serverUrl = "https://nmamgu2cjaat.usemoralis.com:2053/server";
 const appId = "pDj5JIBr9krM14yR5463zuby4J48S6NJ3pRsEfxP";
 Moralis.start({ serverUrl, appId });
 
+signUp = async (email, password) => {
+    const user = new Moralis.User();
+    user.set('username', email);
+    user.set('email', email);
+    user.set('password', password);
+    const btnAvatar = $('#btnAvatar')[0];
+    if (btnAvatar.files.length > 0) {
+        const file = btnAvatar.files[0];
+        const fileName = 'avatar.jpg';
+        const avatarFile = new Moralis.File(FileName, file);
+        
+        /** object.set('variable_name', value); */
+        user.set('avatar', avatarFile);
+    }
+
+    try {
+        await user.signUp();
+    } catch (error) {
+        const code = error.code;
+        const message = error.message;
+    }
+};
+
 /* Add Moralis Authentication code */
 async function login() {
     let user = Moralis.User.current();
@@ -22,6 +45,10 @@ async function logOut() {
     await Moralis.User.logOut();
     console.log("logged out");
 }
+
+Moralis.Cloud.define('getItems', async (request) => {
+    const query = new Moralis.Query('Item');
+});
 
 document.getElementById("btn-login").onclick = login;
 document.getElementById("btn-logout").onclick = logOut;
